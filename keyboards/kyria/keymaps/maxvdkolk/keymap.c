28fcc12b21333f5481aa04f9ef6a968e287de59f
Max van der Kolk <m.vanderkolk@uva.nl>
2021-05-05 11:07:58 +0200

    Updated layout

--- keyboards/kyria/keymaps/maxvdkolk/keymap.c
+++ keyboards/kyria/keymaps/maxvdkolk/keymap.c
@@ -33,15 +33,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Del  | GUI  | LCTL | Bksp | Esc  |  | Tab  | Enter| Space| Bksp | AltGr|
- *                        |      |      |      | Lower| Raise|  | Raise| Lower|      |      |      |
+ *                        | Del  | GUI  | Bksp | LCTL | Esc  |  | Tab  | Enter| Space| Bksp | AltGr|
+ *                        |      |      | LOWER|      | Raise|  | Raise|LShift| Lower|      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
       LT(_RAISE, KC_ESC),       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
       KC_ESC,                   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT,                  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-              KC_DEL, KC_LGUI, KC_LCTL, LT(_LOWER, KC_BSPC), LT(_RAISE, KC_ESC), LT(_RAISE, KC_TAB), LT(_LOWER, KC_ENT), KC_SPC,  KC_BSPC, KC_RALT
+              KC_DEL, KC_LGUI, LT(_LOWER, KC_BSPC), KC_LCTL, LT(_RAISE, KC_ESC), LT(_RAISE, KC_TAB), MT(MOD_LSFT, KC_ENT),  LT(_LOWER, KC_SPC),  KC_BSPC, KC_RALT
     ),
 /*
  * Lower Layer: Symbols
@@ -67,21 +67,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Raise Layer: Number keys, media, navigation
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |   1  |  2   |  3   |  4   |  5   |                              | Home | PgUp | PgDn | End  |  0   |        |
+ * |        |   1  |  2   |  3   |  4   |  5   |                              | Home | PgDn | PgUp | End  |  0   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |        |      | Prev | Play | Next | VolUp|                              | Left | Down | Up   | Right|      |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |      |      |      | Mute | VolDn|      |      |  |      |      | MLeft| Mdown| MUp  |MRight|      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
+ *                        |      |      |      |      |      |  |      |      | MLBTN| MRBTN|      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_RAISE] = LAYOUT(
-      _______, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                      KC_HOME, KC_PGUP, KC_PGDN, KC_END,    KC_0,    _______,
+      _______, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                      KC_HOME, KC_PGDN, KC_PGUP, KC_END,    KC_0,    _______,
       _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
       _______, _______, _______, _______, KC_MUTE, KC_VOLD, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______
     ),
 /*
  * Adjust Layer: Function keys, RGB
